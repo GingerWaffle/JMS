2c9bb704d450ca2a36758905ef5ffc786d452912
Svitlana Kuznietsova <svitlana.kuznietsova@travisperkins.co.uk>
2015-04-28 09:35:13 +0300

    initial commit

---
+++ src/main/java/controller/Greeter.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class Greeter {
+  public String sayHello() {
+    return "Hello world!";
+  }
+}
---
+++ src/main/java/controller/StartEntryController.java
@@ -0,0 +1,8 @@
+package controller;
+
+public class StartEntryController {
+  public static void main(String[] args) {
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
+}
